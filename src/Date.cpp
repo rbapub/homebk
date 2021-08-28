a5ec91ba74d236bdb694f81b88cfc7d8b8d3a1f4
rbapub <rba.pub@gmail.com>
2021-08-28 19:04:07 +0300

    Delete cyrilic character

--- src/Date.cpp
+++ src/Date.cpp
@@ -2,31 +2,29 @@
 #include <ctime>
 #include "Date.h"
 
-//инициализировать статический элемент в области действия файла
-//массив последних дней месяцев
+// month's days
 const int Date::days[] =
 { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
-//конструктор Date
+// ctor Date
 Date::Date(int y, int m, int d, int h, int min, int sec)
 {
 	setDate(y, m, d, h, min, sec);
 }
 
-//конструктор Date на основании данных из функции localtime()
+// ctor Date on localtime()
 Date::Date(tm *ti)
 {
 	setDate(ti->tm_year + 1900, ti->tm_mon + 1, ti->tm_mday,
 		ti->tm_hour, ti->tm_min, ti->tm_sec);
 }
 
-//установить год, месяц, день
+// set year, month, day
 void Date::setDate(int yy, int mm, int dd, int h, int min, int sec)
 {
 	year = (yy >= 1900 && yy <= 2100) ? yy : 1900;
 	month = (mm >= 1 && mm <= 12) ? mm : 1;
 
-	//проверить, високосный-ли год и установить день
 	if (month == 2 && leapYear(year))
 		day = (dd >= 1 && dd <= 29) ? dd : 1;
 	else
@@ -37,49 +35,42 @@ void Date::setDate(int yy, int mm, int dd, int h, int min, int sec)
 	second = (sec >= 0 && sec <= 59) ? sec : 0;
 }
 
-//перегруженная операция префиксного инкремента
 Date &Date::operator++()
 {
-	helpIncrement(); //инкрементировать дату
-	return *this; //возврат ссылки для получения lvalue
+	helpIncrement();
+	return *this; // return reference for lvalue
 }
 
-//перегруженная операция постфиксного инкремента;
-//для целого параметра-пустышки нет имени параметра
 Date Date::operator++(int)
 {
-	//запомнить текущее состояние объекта
 	Date temp = *this;
 	helpIncrement();
 
-	//вернуть неинкрементированный сохраненный объект
-	return temp; //возврат значения, не ссылки
+	return temp;
 }
 
-//прибавить к дате указанное число дней
 const Date &Date::operator+=(int additionalDays)
 {
 	for (int i = 0; i < additionalDays; i++)
 		helpIncrement();
 
-	return *this; //разрешает каскадирование
+	return *this; // allow cascade
 }
 
-//если високосный год, возвратить true; иначе - false
+// for leap year return true, else - false
 bool Date::leapYear(int testYear) const
 {
 	if (testYear % 400 == 0 ||
 		(testYear % 100 != 0 && testYear % 4 == 0))
-		return true; //високосный год
+		return true;
 	else
 		return false;
 }
 
-//определить, является-ли день последним днем месяца
 bool Date::endOfMonth(int testDay) const
 {
 	if (month == 2 && leapYear(year))
-		//последний день февраля високосного года
+		// last day of february of leap year
 		return testDay == 29;
 	else
 		return testDay == days[month];
@@ -115,31 +106,26 @@ int Date::getSecond() const
 	return second;
 }
 
-//вспомогательная функция для инкремента даты
 void Date::helpIncrement()
 {
-	//не последний день месяца
 	if (!endOfMonth(day))
-		//инкрементировать день
 		day++;
 	else
 	{
-		//последний день месяца и месяц <12
 		if (month < 12)
 		{
-			month++; //инкрементировать месяц
-			day = 1; //первый день нового месяца
+			month++;
+			day = 1;
 		}
-		else //последний день года
+		else // last day of year
 		{
-			year++; //инкрементировать год
-			month = 1; //первый месяц нового года
-			day = 1; //первый день нового месяца
+			year++;
+			month = 1;
+			day = 1;
 		}
 	}
 }
 
-//перегруженная операция вывода
 ostream &operator<<(ostream &output, const Date &d)
 {
 	static const char* monthName[13] =
--- src/Date.h
+++ src/Date.h
@@ -7,14 +7,14 @@ class Date
 {
 	friend ostream &operator<<(ostream &, const Date &);
 	public:
-		Date(int y=1900, int m=1, int d=1, int h=0, int min=0, int sec=0); //конструктор по умолчанию
+		Date(int y=1900, int m=1, int d=1, int h=0, int min=0, int sec=0); // default ctor
 		Date(tm *ti);
-		void setDate(int, int, int, int, int, int); //установить дату
-		Date &operator++(); //операция префиксного инкремента
-		Date operator++(int); //операция постфиксного инкремента
-		const Date &operator+=(int); //прибавить дни к дате
-		bool leapYear(int) const; //проверка на високосный год
-		bool endOfMonth(int) const; //проверка на последний день месяца
+		void setDate(int, int, int, int, int, int);
+		Date &operator++();
+		Date operator++(int);
+		const Date &operator+=(int); // add days to date
+		bool leapYear(int) const; // leap year test
+		bool endOfMonth(int) const; // last day of month test
 		int getYear() const;
 		int getMonth() const;
 		int getDay() const;
@@ -28,8 +28,8 @@ class Date
 		int hour;
 		int minute;
 		int second;
-		static const int days[]; //массив дней в месяцах
-		void helpIncrement(); //сервисная функция для инкремента даты
+		static const int days[]; // days in monthes
+		void helpIncrement(); // date's increment service function
 };
 
 #endif
